2842b4aa4e2b27289beb95990d8c96c63b070a20
MikeBorsch <michaelcborsch@gmail.com>
2025-04-13 12:32:49 -0400

    changes

---
+++ testfile.tf
@@ -0,0 +1,101 @@
+
+provider "aws" {
+  region = "us-east-1"
+  alias = "east"
+}
+
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+}
+
+# Security Group
+resource "aws_security_group" "instance_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# S3 Buckets
+resource "aws_s3_bucket" "bucket1" {
+  bucket = "my-terraform-bucket-1"
+}
+
+resource "aws_s3_bucket" "bucket2" {
+  bucket = "my-terraform-bucket-2"
+}
+
+resource "aws_s3_bucket" "bucket3" {
+  bucket = "my-terraform-bucket-3"
+}
+
+# IAM Role for EC2 to access S3
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_s3_access_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_policy_attachment" "s3_access" {
+  name       = "s3-access"
+  roles      = [aws_iam_role.ec2_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2_instance_profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+# EC2 Instances
+resource "aws_instance" "web1" {
+  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.subnet_a.id
+  security_groups = [aws_security_group.instance_sg.name]
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+
+  tags = {
+    Name = "WebServer1"
+  }
+}
+
+resource "aws_instance" "web2" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.subnet_a.id
+  security_groups = [aws_security_group.instance_sg.name]
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+
+  tags = {
+    Name = "WebServer2"
+  }
+}
+
+output "s3_buckets" {
+  value = [aws_s3_bucket.bucket1.bucket, aws_s3_bucket.bucket2.bucket, aws_s3_bucket.bucket3.bucket]
+}
+
+output "ec2_instance_ips" {
+  value = [aws_instance.web1.public_ip, aws_instance.web2.public_ip]
+}
